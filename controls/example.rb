de301a451f2ac7d7577f019b72d308bb1d3117c0
Jeremy J. Miller <jm@chef.io>
2017-07-24 12:33:05 -0400

    attribute
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- controls/example.rb
+++ controls/example.rb
@@ -2,6 +2,12 @@
 # copyright: 2015, The Authors
 # license: All rights reserved
 
+val_password = attribute('password', description: 'A value for the password')
+
+describe val_password do
+  it { should eq 'bob' }
+end
+
 control 'tmp-1.0' do
   title 'Create /tmp directory'             # A human-readable title
   desc  "The /tmp directory is a world-writable directory used for temporary storage by all users and some applications."
